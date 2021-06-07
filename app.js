8293e4327b7dbe921774b51f666b979619d39b74
ZeusCodes <paulpallab1112@gmail.com>
2021-06-07 22:32:47 +0530

    Encrypted Using Hash md5

--- app.js
+++ app.js
@@ -3,73 +3,83 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 const _ = require("lodash");
 
 const app = express();
 app.use(express.static("public"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true, useUnifiedTopology: true})
-
-const userSchema = mongoose.Schema({
-  emailId : String,
-  password : String,
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"] });
+const userSchema = mongoose.Schema({
+  emailId: String,
+  password: String,
+})
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.render("home");
 })
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 })
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 })
 
 
-app.get("/submit",function(req,res){
+app.get("/submit", function(req, res) {
   res.render("submit");
 })
 
-app.get("/logout",function(req,res){
+app.get("/logout", function(req, res) {
   res.redirect("/");
 })
 ///////////////POST ROUTES/////////////////
 
-app.post("/login",function(req,res){
-  const username = req.body.username;
-  User.find({emailId:req.body.username,password:req.body.password},function(err,foundUser){
-    if(foundUser){
-      res.render("secrets");
-    }else{
-      res.send("User Not Found. Try Again!!");
-    }
-  })
-})
-
-app.post("/register",function(req,res){
-  const newUser = new User({
-    emailId : req.body.username,
-    password : req.body.password,
-  });
-  newUser.save(function(err){
-    if(err){
+app.post("/login", function(req, res) {
+  User.findOne({emailId: req.body.username}, function(err, foundUser) {
+    if (err) {
       console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === md5(req.body.password)) {
+          console.log("Logged In");
+          res.render("secrets");
+        }else{
+          res.send("Incorrect Password. Try Again!!");
+        }
+      } else {
+        res.send("User Not Found. Try Again!!");
+      }
     }
-    else{
+  })
+})
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    emailId: req.body.username,
+    password: md5(req.body.password),
+  });
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
       res.render("secrets");
     }
   })
 })
 
-app.listen(process.env.PORT ||3000,function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log("Port Listening on 3000")
 })
